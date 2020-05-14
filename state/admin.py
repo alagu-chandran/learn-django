db77cc49439c4e8015769a8f3a50a4df14a96ca8
Alagu Chandran <alagu.chandran@freshworks.com>
2020-05-14 19:34:39 +0530

    Added function to show mulitple fields data

--- state/admin.py
+++ state/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from . models import State
 
-# Register your models here.
+
+class StateAdmin(admin.ModelAdmin):
+    list_display = ('name', 'active', 'created_on')
+
+    def active(self, obj):
+        return obj.is_active == 1
+    
+    active.boolean = True
+
+admin.site.register(State, StateAdmin)
--- state/models.py
+++ state/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class State(models.Model):
+    name = models.CharField(max_length=50)
+    is_active = models.IntegerField(default=1, 
+                                    blank=True,
+                                    null=True,
+                                    help_text='1-> Active, 0-> Inactive',
+                                    choices=((1, 'Active'), (0, 'Inactive')
+                                    ))
+    created_on = models.DateTimeField(default = timezone.now)
+    updated_on = models.DateTimeField(default = timezone.now, 
+                                    null = True,  
+                                    blank = True
+                                    )
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'State List'
+        db_table = 'state'
