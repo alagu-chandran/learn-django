e936bfa7a876ac7be43ca6a88425177b1655b79a
Alagu Chandran <alagu.chandran@freshworks.com>
2020-05-14 19:53:04 +0530

    Added auxilary functions

--- state/admin.py
+++ state/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib import messages
 from . models import State
 
 
@@ -7,7 +8,22 @@ class StateAdmin(admin.ModelAdmin):
 
     def active(self, obj):
         return obj.is_active == 1
-    
     active.boolean = True
 
+    def make_active(modeadmin, request, queryset):
+        queryset.update(is_active = 1)
+        messages.success(request, "Selected Record(s) Marked as Active")
+
+    def make_inactive(modeadmin, request, queryset):
+        queryset.update(is_active = 0)
+        messages.success(request, "Selected Record(s) Marked as Inactive")
+    
+    def has_delete_permission(self, request, obj=None): #Add/ Remove according to the User Permissions
+        return False
+    
+    def has_add_permission(self, request): #Add/ Remove according to the User Permissions
+        return False
+    
+    admin.site.add_action(make_active, "Make Active")
+    admin.site.add_action(make_inactive, "Make Inactive")
 admin.site.register(State, StateAdmin)
