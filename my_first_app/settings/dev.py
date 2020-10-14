336342817e691af0e122bf40a3a87ef413633101
Alagu Chandran <alagu.chandran@freshworks.com>
2020-10-14 19:58:07 +0530

    settings file made for env

---
+++ my_first_app/settings/dev.py
@@ -0,0 +1,14 @@
+from my_first_app.settings.base import *
+
+DEBUG = True
+
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
